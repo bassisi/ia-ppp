a1e14976e770878f230e627d8aa7a281e816efaa
Renato Bassi <renato.bassi@alumnos.inf.utfsm.cl>
2020-12-29 13:29:10 -0300

    SA aparentemente terminado

--- sabalero.py
+++ sabalero.py
@@ -2,34 +2,35 @@ import random
 import math
 import numpy as np
 
-def simulated_annealing(initial_state):
+def simulated_annealing(initial_state, hosts):
     """Peforms simulated annealing to find a solution"""
     initial_temp = 90
-    final_temp = .1
+    final_temp = 0
     alpha = 0.01
     
     current_temp = initial_temp
 
     # Start by initializing the current state with the initial state
-    current_state = initial_state
-    solution = current_state
+    current_state = initial_state.copy()
+    solution = current_state.copy()
 
-    while current_temp > final_temp:
-        neighbor = random.choice(get_neighbors())
+    while current_temp > final_temp and get_cost(solution) != 0:
+        neighbor = random.choice(get_neighbors(current_state, hosts)+get_neighbors2(current_state)).copy() 
 
+        print(get_cost(neighbor), current_temp)
         # Check if neighbor is best so far
         cost_diff = get_cost(current_state) - get_cost(neighbor)
-
         # if the new solution is better, accept it
         if cost_diff > 0:
-            solution = neighbor
+            current_state = neighbor.copy()
         # if the new solution is not better, accept it with a probability of e^(-cost/temp)
         else:
             if random.uniform(0, 1) < math.exp(cost_diff / current_temp):
-                solution = neighbor
+                current_state = neighbor.copy()
+        if get_cost(solution) > get_cost(current_state):
+            solution = current_state.copy()
         # decrement the temperature
         current_temp -= alpha
-
     return solution
 
 def diff_cost(solucion):
@@ -41,7 +42,7 @@ def diff_cost(solucion):
                     costodiff += 1
     return costodiff/2
 
-def meet_cost(solucion):
+def once_cost(solucion):
     costomeet = 0
     guests = dict()
     for j in range(solucion.shape[1]):
@@ -69,15 +70,76 @@ def meet_cost(solucion):
         costomeet += len(guests[guest]) - len(guestset)
     
     return costomeet/2
-    
 
-def get_cost(state):
-    """Calculates cost of the argument state for your solution."""
-    raise NotImplementedError
-    
-def get_neighbors(state):
-    """Returns neighbors of the argument state for your solution."""
-    raise NotImplementedError
+def capa_cost(solution):
+    capa_cost = 0
+    for j in range(solution.shape[1]):
+        hosts = dict()
+        columna = solution[:,j]
+        i = 0
+        for host in columna:
+            if host not in hosts:
+                hosts[int(host)] = [huespedes[i]]
+            else:
+                hosts[int(host)] += [huespedes[i]]
+            i += 1
+
+        for host in hosts:
+            cap_rest = instancia["D"][host][0]-instancia["D"][host][1]
+            crews_size = 0
+            for guest in hosts[host]:
+                crews_size += instancia["D"][guest][1]
+            sigma = cap_rest - crews_size
+            if sigma <= 0:
+                sigma = abs(sigma)
+            else:
+                sigma = 0
+            capa_cost += 1 + (sigma-1)/4
+
+    return capa_cost
+
+def get_cost(solution):
+    total_cost = 4*diff_cost(solution) + 2*once_cost(solution) + 4*capa_cost(solution)   
+    return total_cost
+
+def get_neighbors(solution, hosts):
+    neighbors = list()
+    for i in range(solution.shape[0]):
+        for j in range(solution.shape[1]):
+            for host in hosts:
+                solutionaux = solution.copy()
+                if host != solution[i,j]:
+                    solutionaux[i,j] = host
+                    neighbors.append(solutionaux)
+    return neighbors
+
+def get_neighbors2(solution):
+    neighbors = list()
+    for i in range(solution.shape[1]):
+        columna = solution[:,i].copy()
+        for j in range(len(columna)):
+            solucion_aux = solution.copy()
+            for k in range(j,len(columna)):
+                if(k != j):
+                    aux = columna[j].copy()
+                    columna[j] = columna[k].copy()
+                    columna[k] = aux.copy()
+                    solucion_aux[:,i] = columna.copy()
+                    neighbors.append(solucion_aux.copy())
+                columna = solution[:,i].copy()
+
+    return neighbors
+
+def best_neighbor(neighbors):
+    solution = np.zeros(neighbors[0].shape)
+    min_cost = float("inf")
+
+    for neighbor in neighbors:
+        cost = get_cost(neighbor)
+        if cost < min_cost:
+            min_cost = cost
+            solution = neighbor.copy()
+    return solution
 
 archivo = open("./Configuraciones/PPP.txt")
 instancia = dict()
@@ -110,10 +172,7 @@ for i in range(len(huespedes)):
     for j in range(instancia["T"]):
         solucion_i[i][j] = np.random.choice(list(anfitriones))
 
-print(solucion_i)
-
-print(solucion_i[:,0])
-
-print(meet_cost(solucion_i))
+#solucion_i = np.matrix([[1,2,3],[2,3,4],[3,4,5]])
+print(simulated_annealing(solucion_i, anfitriones))
 
 
